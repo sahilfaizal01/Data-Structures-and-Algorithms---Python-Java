522eac129f0e4d9833c749d11edaf57ca7aefd89
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-02-06 00:18:31 -0400

    min of three nums

---
+++ 3.min-of-three-nums.py
@@ -0,0 +1,9 @@
+a = 14
+b = 2
+c = 12
+if((a>b and b>c)or(c<a and a<b)):
+    print('c is smallest')
+elif((b>a and c>a)or(a<b and b<c)):
+    print('a is smallest')
+elif((c>a and a>b)or(a>c and c>b)):
+    print('b is smallest')
