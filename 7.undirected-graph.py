e27f46c3d20196c8feb0c5a079ed55fd6fb7470a
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-02-12 02:20:07 -0400

    Create 7.undirected-graph.py

---
+++ 7.undirected-graph.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+    def insertEdge(self,v1,v2):
+        self.graph[v1].append(v2)
+        self.graph[v2].append(v1)
+    def printGraph(self):
+        for node in self.graph:
+            for v in self.graph[node]:
+                print(node,'->',v)
+
+g = Graph()
+g.insertEdge(1,5)
+g.insertEdge(5,2)
+g.insertEdge(2,7)
+g.insertEdge(7,1)
+
+g.printGraph()
