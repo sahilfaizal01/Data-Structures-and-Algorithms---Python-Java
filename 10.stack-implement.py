90e7a95cbff1193b7f1b65d385d8889dce3b2999
Sahil Faizal <106440078+sahilfaizal01@users.noreply.github.com>
2023-02-16 12:46:00 +0530

    Feb16,2023

---
+++ 10.stack-implement.py
@@ -0,0 +1,28 @@
+stack = []
+
+def push():
+  ele = input('Enter the element:')
+  stack.append(ele)
+  print(stack)
+
+def pop_element():
+  if not stack:
+    print('Stack is Empty')
+  else:
+    e = stack.pop()
+    print('Removed Element is ',e)
+    print(stack)
+
+while True:
+  print('SELECT THE Operation: 1) Push 2)Pop 3)Top/Peek 4)Exit')
+  choice = int(input())
+  if choice==1:
+    push()
+  elif choice==2:
+    pop_element()
+  elif choice==3:
+    print('Top element is ',stack[-1])
+  elif choice==4:
+    break
+  else:
+    print('ENTER the correct option')
